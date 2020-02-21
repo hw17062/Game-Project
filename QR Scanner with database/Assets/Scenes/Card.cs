22becf5df685ecec4da37792cef192078207f492
Jonah Lyu <lvjikeleo@hotmail.com>
2020-02-21 11:35:50 +0000

    QR Scanner with database

---
+++ QR Scanner with database/Assets/Scenes/Card.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+public class Card
+{
+    public ObjectId Id { get; set; }
+    public string cardname { get; set; }
+    public int target { get; set; }
+    public double heal { get; set; }
+    public int cost { get; set; }
+    
+}
